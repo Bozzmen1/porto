b1d4670f018d607f334aa0294c0876b83b0eca0d
Cindy <wijayacindy79@gmail.com>
2025-07-28 17:31:11 +0700

    Hero Section

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+
+export default {
+  content: [
+     "./index.html"
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '4rem', 
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
